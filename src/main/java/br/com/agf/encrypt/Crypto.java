926097cae5bedf4da521d78c27fb2432c969727a
allan.freitas <allan.freitas@DESKTOP-CC4Q6EF.arqdigital.local>
2016-08-23 10:25:23 -0300

    Alterações nas classes...apenas identação.

--- src/main/java/br/com/agf/encrypt/Crypto.java
+++ src/main/java/br/com/agf/encrypt/Crypto.java
@@ -25,14 +25,14 @@ public class Crypto {
 
     public static String Encrypt(String text, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        byte[] keyBytes= new byte[16];
-        byte[] b= key.getBytes("UTF-8");
-        int len= b.length;
+        byte[] keyBytes = new byte[16];
+        byte[] b = key.getBytes("UTF-8");
+        int len = b.length;
         if (len > keyBytes.length) len = keyBytes.length;
         System.arraycopy(b, 0, keyBytes, 0, len);
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
         IvParameterSpec ivSpec = new IvParameterSpec(keyBytes);
-        cipher.init(Cipher.ENCRYPT_MODE,keySpec,ivSpec);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 
         byte[] results = cipher.doFinal(text.getBytes("UTF-8"));
         return Base64.encodeBase64String(results);
