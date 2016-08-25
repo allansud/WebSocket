a9510329cf993cfa3cb31a7a554ef0add9459437
allan.freitas <allan.freitas@DESKTOP-CC4Q6EF.arqdigital.local>
2016-08-25 16:52:00 -0300

    Daos...

--- src/main/java/br/com/agf/dao/UsuarioDao.java
+++ src/main/java/br/com/agf/dao/UsuarioDao.java
@@ -1,5 +1,27 @@
 package br.com.agf.dao;
 
+import javax.persistence.EntityManager;
+
+import com.google.inject.Inject;
+
+import br.com.agf.domain.Usuario;
+
 public class UsuarioDao {
 
+	protected EntityManager entityManager;
+	
+	@Inject
+	public UsuarioDao(EntityManager entityManager){
+		this.entityManager = entityManager;
+	}
+	
+	public void saveUsuarioInNewTransaction(Usuario usuario){
+		entityManager.getTransaction().begin();
+		save(usuario);
+		entityManager.getTransaction().commit();
+	}
+	
+	public void save(Usuario usuario){
+		entityManager.persist(usuario);
+	}
 }
---
+++ src/main/java/br/com/agf/module/UsuarioDaoModule.java
@@ -0,0 +1,47 @@
+package br.com.agf.module;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class UsuarioDaoModule extends AbstractModule {
+
+	// EntityManager para cada Thread
+	private static final ThreadLocal<EntityManager> ENTITY_MANAGER_CACHE = new ThreadLocal<EntityManager>();
+
+	@Override
+	protected void configure() {
+	}
+	
+	@Provides
+	@Singleton
+	public EntityManagerFactory provideEntityManagerFactory() {
+
+		Map<String, String> properties = new HashMap<String, String>();
+		properties.put("hibernate.connection.driver_class", "org.h2.Driver");
+		properties.put("hibernate.connection.url", "jdbc:h2:test");
+		properties.put("hibernate.connection.username", "sa");
+		properties.put("hibernate.connection.password", "");
+		properties.put("hibernate.connection.pool_size", "1");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.hbm2ddl.auto", "create");
+
+		return Persistence.createEntityManagerFactory("db-manager", properties);
+	}
+	
+	@Provides
+	public EntityManager provideEntityManager(EntityManagerFactory entityManagerFactory) {
+		EntityManager entityManager = ENTITY_MANAGER_CACHE.get();
+		if (entityManager == null) {
+			ENTITY_MANAGER_CACHE.set(entityManager = entityManagerFactory.createEntityManager());
+		}
+		return entityManager;
+	}
+}
