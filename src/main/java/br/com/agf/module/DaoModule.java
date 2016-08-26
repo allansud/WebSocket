c68bbbb7b02d20aa1ca801f9ae3fcda86dca0991
allan.freitas <allan.freitas@DESKTOP-CC4Q6EF.arqdigital.local>
2016-08-26 11:38:59 -0300

    Alterações no module.

--- src/main/java/br/com/agf/module/DaoModule.java
+++ src/main/java/br/com/agf/module/DaoModule.java
@@ -11,7 +11,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 
-public class UsuarioDaoModule extends AbstractModule {
+public class DaoModule extends AbstractModule {
 
 	// EntityManager para cada Thread
 	private static final ThreadLocal<EntityManager> ENTITY_MANAGER_CACHE = new ThreadLocal<EntityManager>();
@@ -25,12 +25,12 @@ public class UsuarioDaoModule extends AbstractModule {
 	public EntityManagerFactory provideEntityManagerFactory() {
 
 		Map<String, String> properties = new HashMap<String, String>();
-		properties.put("hibernate.connection.driver_class", "org.h2.Driver");
-		properties.put("hibernate.connection.url", "jdbc:h2:test");
+		properties.put("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+		properties.put("hibernate.connection.url", "jdbc:mysql://192.168.0.104:3306/MobileChat");
 		properties.put("hibernate.connection.username", "sa");
 		properties.put("hibernate.connection.password", "");
 		properties.put("hibernate.connection.pool_size", "1");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
 		properties.put("hibernate.hbm2ddl.auto", "create");
 
 		return Persistence.createEntityManagerFactory("db-manager", properties);
