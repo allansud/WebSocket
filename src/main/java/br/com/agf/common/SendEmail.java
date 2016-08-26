340f7323c6e356033895754dcc46242d10d26213
allan.freitas <allan.freitas@DESKTOP-CC4Q6EF.arqdigital.local>
2016-08-26 12:21:37 -0300

    SendEmail.

---
+++ src/main/java/br/com/agf/common/SendEmail.java
@@ -0,0 +1,53 @@
+package br.com.agf.common;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class SendEmail {
+
+	public void Enviar() {
+		try {
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	protected String readEmailFromHtml(String filePath, Map<String, String> input) {
+		String msg = readContentFromFile(filePath);
+		try {
+			Set<Entry<String, String>> entries = input.entrySet();
+			for (Map.Entry<String, String> entry : entries) {
+				msg = msg.replace(entry.getKey().trim(), entry.getValue().trim());
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		return msg;
+	}
+
+	private String readContentFromFile(String fileName) {
+		StringBuffer contents = new StringBuffer();
+
+		try {
+			// Usnado o buffer para ler uma linha de cada vez.
+			BufferedReader reader = new BufferedReader(new FileReader(fileName));
+			try {
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					contents.append(line);
+					contents.append(System.getProperty("line.separator"));
+				}
+			} finally {
+				reader.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return contents.toString();
+	}
+}
